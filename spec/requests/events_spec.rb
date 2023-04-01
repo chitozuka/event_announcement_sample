2930ea19cc5fe40ad83bfd8d72cd9461b201ddc8
Hajime Iizuka <chitozuka@gmail.com>
2023-04-01 23:08:36 +0900

    event作成のrspec

--- spec/requests/events_spec.rb
+++ spec/requests/events_spec.rb
@@ -1,7 +1,25 @@
 require 'rails_helper'
 
-RSpec.describe "Events", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe 'Events', type: :request do
+  describe 'GET #new' do
+    before { get auth_callback_path(provider: 'github') }
+
+    let(:valid_event_params) { FactoryBot.attributes_for(:event) }
+
+    it 'return http success' do
+      get new_event_path
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'create a new event' do
+      expect do
+        post events_path, params: { event: valid_event_params }
+      end.to change(Event, :count).by(1)
+    end
+
+    it 'redirects to top page' do
+      post events_path, params: { event: valid_event_params }
+      expect(response).to redirect_to(event_path(Event.last))
+    end
   end
 end
