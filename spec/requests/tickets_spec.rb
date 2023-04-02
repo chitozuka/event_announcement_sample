609c46b629e4237bc2fc10ea7ff91952fa94898b
Hajime Iizuka <chitozuka@gmail.com>
2023-04-02 18:21:36 +0900

    Ticketのrequest specを追加

--- spec/requests/tickets_spec.rb
+++ spec/requests/tickets_spec.rb
@@ -1,7 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe "Tickets", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe 'Tickets', type: :request do
+  describe 'POST #new' do
+    context 'when the user is logged in' do
+      before { get auth_callback_path(provider: 'github') }
+      let(:event) { FactoryBot.create(:event) }
+      let(:params) { FactoryBot.attributes_for(:ticket) }
+
+      it 'create a new ticket' do
+        expect do
+          post event_tickets_path(event.id), params: { ticket: params }
+        end.to change(Ticket, :count).by(1)
+      end
+    end
   end
 end
